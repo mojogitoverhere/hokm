7757e8c2c6bdbd9fe49d0fb9042c53f272aeac1a
David Morales <david.morales@connexta.com>
2021-09-25 18:32:59 -0700

    Wrap setup in form

--- src/components/Setup.vue
+++ src/components/Setup.vue
@@ -1,52 +1,54 @@
 <template>
     <div class="main-card">
-        <h1>Setup New Game</h1>
-        <h2>Team Setup</h2>
-        <p>Enter the name of each player in counter-clockwise order based on where they are seated at the table.</p>
-        <div id="table">
-            <div>
-                <label for="player-1">Player 1</label>
-                <input type="text" id="player-1" v-model="player1"/>
+        <form>
+            <h1>Setup New Game</h1>
+            <h2>Team Setup</h2>
+            <p>Enter the name of each player in counter-clockwise order based on where they are seated at the table.</p>
+            <div id="table">
+                <div>
+                    <label for="player-1">Player 1</label>
+                    <input type="text" id="player-1" v-model="player1"/>
+                </div>
+                <div>
+                    <label for="player-2">Player 2</label>
+                    <input type="text" id="player-2" v-model="player2" />
+                </div>
+                <div>
+                    <label for="player-3">Player 3</label>
+                    <input type="text" id="player-3" v-model="player3" />
+                </div>
+                <div>
+                    <label for="player-4">Player 4</label>
+                    <input type="text" id="player-4" v-model="player4" />
+                </div>
             </div>
-            <div>
-                <label for="player-2">Player 2</label>
-                <input type="text" id="player-2" v-model="player2" />
-            </div>
-            <div>
-                <label for="player-3">Player 3</label>
-                <input type="text" id="player-3" v-model="player3" />
-            </div>
-            <div>
-                <label for="player-4">Player 4</label>
-                <input type="text" id="player-4" v-model="player4" />
-            </div>
-        </div>
-        <h2>Score to Win</h2>
-        <label for="score-to-win">Score</label>
-        <input type="number" id="score-to-win" name="score-to-win" v-model="scoreToWin"/>
+            <h2>Score to Win</h2>
+            <label for="score-to-win">Score</label>
+            <input type="number" id="score-to-win" name="score-to-win" v-model="scoreToWin"/>
 
-        <h2>Who is dealing first?</h2>
-        <p v-if="someNamesBlank">Make sure all 4 player's has been entered above first.</p>
-        <div v-else id="dealers">
-            <div class="dealer">
-                <input type="radio" name="dealer" id="player-1" />
-                <label for="player-1">{{player1}}</label>
+            <h2>Who is dealing first?</h2>
+            <p v-if="someNamesBlank">Make sure all 4 player's has been entered above first.</p>
+            <div v-else id="dealers">
+                <div class="dealer">
+                    <input type="radio" name="dealer" id="player-1" />
+                    <label for="player-1">{{player1}}</label>
+                </div>
+                <div class="dealer">
+                    <input type="radio" name="dealer" id="player-2" />
+                    <label for="player-2">{{player2}}</label>
+                </div>
+                <div class="dealer">
+                    <input type="radio" name="dealer" id="player-3" />
+                    <label for="player-3">{{player3}}</label>
+                </div>
+                <div class="dealer">
+                    <input type="radio" name="dealer" id="player-4" />
+                    <label for="player-4">{{player4}}</label>
+                </div>
             </div>
-            <div class="dealer">
-                <input type="radio" name="dealer" id="player-2" />
-                <label for="player-2">{{player2}}</label>
-            </div>
-            <div class="dealer">
-                <input type="radio" name="dealer" id="player-3" />
-                <label for="player-3">{{player3}}</label>
-            </div>
-            <div class="dealer">
-                <input type="radio" name="dealer" id="player-4" />
-                <label for="player-4">{{player4}}</label>
-            </div>
-        </div>
 
-        <button v-on:click="logButton">START NEW GAME</button>
+            <input id="start-new-game-button" type="submit" v-on:click="logButton" value="Start New Game" />
+        </form>
     </div>
 </template>
 
@@ -81,7 +83,6 @@ export default {
 
 <style scoped>
 .main-card {
-    /* border: 1px red solid; */
     box-shadow: 5px 10px 10px #CCCCCC;
     padding: 3em;
     border-radius: 1.5em;
@@ -111,7 +112,7 @@ h1 {
 h2 {
     font-size: 1.25rem;
 }
-button {
+#start-new-game-button {
     display: block;
     padding: 1em 3em;
     margin-top: 2em;
